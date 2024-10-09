a647d4ea46ca58a7b1e9b3ca5a4963813a2f5ddd
mifr2204 <frendin.mikaela@gmail.com>
2024-10-09 16:54:33 +0200

    Allt bör funka

--- server.js
+++ server.js
@@ -14,14 +14,15 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+/*
 mongoose.connection.on('error', err => {
     console.log('mongoose connection error');
     console.dir(err);
 });
-
+*/
 
 //koppla upp till mongo db
-mongoose.connect("mongodb://127.0.0.1:27017/local").then(() => {
+mongoose.connect("mongodb://127.0.0.1:27017/mom3-backend").then(() => {
     console.log("Connected to database");
 
     //console.log(mongoose.connection)
@@ -33,15 +34,15 @@ mongoose.connect("mongodb://127.0.0.1:27017/local").then(() => {
 const WorkplaceSchema = new mongoose.Schema({
     companyname: {
         type: String,
-        required: true,
+        required: [true, "Companyname has to be filled in"],
     },
     location: {
         type: String,
-        required: true,
+        required: [true, "Location has to be filled in"],
     }, 
     startdate: {
         type: Date,
-        required: true,
+        required: [true, "Startdate has to be filled in"],
     },
     enddate: {
         type: Date,
@@ -49,11 +50,11 @@ const WorkplaceSchema = new mongoose.Schema({
     },
     title: {
         type: String,
-        required: true,
+        required: [true, "Title has to be filled in"],
     },
     description: {
         type: String,
-        required: true,
+        required: [true, "Description has to be filled in"],
     } 
 });
 
@@ -68,183 +69,72 @@ app.listen(port, () => {
 app.get("/workplaces", async(req, res) => {
     try {
         let result = await Workplace.find({});
-        console.dir(result);
+        //console.dir(result);
 
         return res.json(result);
     }catch(error) {
         return res.status(500).json(error);
     }
-    /*
-    //Hämta data från db
-    db.all("SELECT * FROM workplaces;", (err, results) => {
-       
-        if(err) {
-          res.status(500).json({error: "Error" + err});
-          return;
-        }
-        if(results.length === 0) {
-            res.status(404).json({message: "No workplaces found"});
-        }else{
-            res.json(results);
-        }
-    })*/
 });
 
+//skickar till databasen
+app.post("/workplaces", async(req, res) => {
+    try {
+        let result = await Workplace.create(req.body);
+
+        return res.json(result);
+    }catch(error) {
+        return res.status(400).json(error);
+    }
+});
+
+app.put("/workplaces/:id", async(req, res) => {
+    try {
+        let id = req.params.id;
+        let companyname = req.body.companyname;
+        let location = req.body.location;
+        let startdate = req.body.startdate;
+        let enddate = req.body.enddate;
+        let title = req.body.title;
+        let description = req.body.description;
 /*
-//Anslutning till databasen
-const db = new sqlite3.Database("./db/CV.db");
-
-
-
-
-
-
+        const filter = {_id: id};
+        const update = {companyname: companyname, location: location, startdate: startdate, enddate: enddate, title: title, description: description};
+        const result = await Workplace.findOneAndUpdate(filter, update, {
+            
+        });*/
+        let result = await Workplace.findOneAndUpdate({_id: id}, 
+        {
+            $set: {
+                companyname: companyname,
+                location: location, 
+                startdate: startdate, 
+                enddate: enddate, 
+                title: title, 
+                description: description
+            },
+        }, 
+        {
+            returnOriginal: false,
+        });
+       
+        return res.json(result);
+        
+    }catch(error) {
+        return res.status(400).json(error);
+    };
+});
 
 
 //radera arbetsplats
-app.delete('/workplaces/:id', (req, res) => {
-  
-  let id = req.params.id;
- 
-  db.all("SELECT * FROM workplaces WHERE id=" + id + ";", (err, results) => {
-  
-    if(err) {
-      res.status(500).json({error: "Error" + err});
-      return;
-    }
-    if(results.length === 0) {
-        res.status(404).json({message: "No workplaces found"});
-    }else{
-      const stmt = db.prepare("DELETE FROM workplaces WHERE id=?;", (err, results) => {
-
-        if(err) {
-          res.status(500).json({error: "Error" + err});
-          return;
-        }
-        stmt.run(id);
-        stmt.finalize();
-
-        res.json({message: "The workplace is deleted ", id});
+app.delete("/workplaces/:id", async(req, res) => {
+    try {
+        let id = req.params.id;
+        let result = await Workplace.deleteOne({_id: id}); 
         
-      })
-    } 
-  }) 
-});
-
-//lägga till arbetsplats
-app.post("/workplaces", (req, res) => {
-    let companyname = req.body.companyname;
-    let location = req.body.location;
-    let startdate = req.body.startdate;
-    let enddate = req.body.enddate;
-    let title = req.body.title;
-    let description = req.body.description;
-
-    //felhantering
-    let errors = {
-        message: "",
-        detail: "",
-        https_response: {
-
-        }
-    }
-
-    if(!companyname || !location || !startdate || !enddate || !title || !description) {
-        
-        errors.message = "All fields need to be filled in"
-        errors.detail = "Företagsnamn, plats, startdatum, slutdatum, titel och beskrivning måste finnas med i JSON"
-        errors.https_response.message = "Bad request";
-        errors.https_response.code = 400;
-        
-        res.status(400).json(errors);
-     
-        return;
-    }
-
-    const stmt = db.prepare('INSERT INTO workplaces(companyname, location, startdate, enddate, title, description) VALUES(?,?,?,?,?,?);',[companyname, location, startdate, enddate, title, description], (err, results) => {
-        if(err){
-            res.status(500).json({error: "something went wrong:" + err});
-            return;
-        }
-        stmt.run(companyname, location, startdate, enddate, title, description);
-        stmt.finalize();
+        res.json({message: "The workplace is deleted ", id});    
     
-        //skapar objekt
-        let workplace = {
-            companyname: companyname,
-            location: location,
-            startdate: startdate,
-            enddate: enddate,
-            title: title,
-            description: description
-        };
-
-        res.json({message: "The workplace is added", workplace});
-    });
+    }catch(error) {
+        return res.status(400).json(error);
+    };
 });
-
-//ändra arbetsplats
-app.put("/workplaces/:id", (req, res) => {
-    let id = req.params.id;
-    let companyname = req.body.companyname;
-    let location = req.body.location;
-    let startdate = req.body.startdate;
-    let enddate = req.body.enddate;
-    let title = req.body.title;
-    let description = req.body.description;
-
-    //felhantering
-    let errors = {
-        message: "",
-        detail: "",
-        https_response: {
-
-        }
-    }
-    console.log("tttt" + companyname + location + enddate + title + description);
-    if(!companyname || !location || !startdate || !enddate || !title || !description) {
-        
-      errors.message = "All fields needs to be filled in"
-      errors.detail = "Företagsnamn, plats, startdatum, slutdatum, titel och beskrivning måste finnas med i JSON"
-      errors.https_response.message = "Bad request";
-      errors.https_response.code = 400;
-      
-      res.status(400).json(errors);
-   
-      return;
-  }
-
-  db.all("SELECT * FROM workplaces WHERE id=" + id + ";", (err, results) => {
-   
-    if(err) {
-      res.status(500).json({error: "Error" + err});
-      return;
-    }
-    if(results.length === 0) {
-        res.status(404).json({message: "No workplaces found"});
-    }else{
-      const stmt = db.prepare('UPDATE workplaces SET companyname=?, location=?, startdate=?, enddate=?, title=?, description=? WHERE id=?;', (err, results) => {
-      if(err){
-          res.status(500).json({error: "something went wrong:" + err});
-          return;
-      }
-      stmt.run(companyname, location, startdate, enddate, title, description, id);
-      stmt.finalize();
-  
-      //skapar objekt
-      let workplace = {
-          companyname: companyname,
-          location: location,
-          startdate: startdate,
-          enddate: enddate,
-          title: title,
-          description: description
-      };
-
-      res.json({message: "The workplace is updated", workplace});
-        
-      })
-    } 
-  }) 
-});
-*/
